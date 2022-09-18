332c343b44f6e2a46d767523b84fe967eb4aa974
LOOKY03 <vajearvin26@gmail.com>
2022-09-18 18:31:54 +0800

    cursorpointer added

--- components/TopNavBar.js
+++ components/TopNavBar.js
@@ -4,13 +4,13 @@ const TopNavBar = () => {
   return (
     <div className="hidden lg:block">
       <div className="flex items-center justify-between mt-10 ">
-        <img src="/images/logo.svg" alt="" />
+        <img src="/images/logo.svg" alt="" className="cursor-pointer"/>
         <div className="flex space-x-5">
-          <h1>Pricing</h1>
-          <h1>Product</h1>
-          <h1>About Us</h1>
-          <h1>Careers</h1>
-          <h1>Community</h1>
+          <h1 className="cursor-pointer">Pricing</h1>
+          <h1 className="cursor-pointer">Product</h1>
+          <h1 className="cursor-pointer">About Us</h1>
+          <h1 className="cursor-pointer">Careers</h1>
+          <h1 className="cursor-pointer">Community</h1>
         </div>
         <button className="bg-orange-500 text-white px-4 py-2 rounded-full">
           Get Started
